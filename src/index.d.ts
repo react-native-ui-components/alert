188263c99158fe36e0d4b08ffb713be785309320
morganbarrett <morganbarrett@icloud.com>
2023-08-07 12:11:36 +0100

    feat: implemented type definitions

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,19 @@
+import { AlertButton, AlertOptions, AlertType } from "react-native";
 
+export declare const Alert: {
+	alert: (
+		title: string,
+		message?: string,
+		buttons?: AlertButton[],
+		options?: AlertOptions,
+	) => void,
+	prompt: (
+		title: string,
+		message?: string,
+		callbackOrButtons?: ((text: string) => void) | AlertButton[],
+		type?: AlertType,
+		defaultValue?: string,
+		keyboardType?: string,
+		options?: AlertOptions,
+	) => void
+}
